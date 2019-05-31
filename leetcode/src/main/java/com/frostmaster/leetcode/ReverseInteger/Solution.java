5798bae8af6e42affaf33d71445d574bffd51451
Igor Usachev <igor_usachev@epam.com>
2019-05-31 10:19:37 +0300

    Reverse integer

---
+++ leetcode/src/main/java/com/frostmaster/leetcode/ReverseInteger/Solution.java
@@ -0,0 +1,31 @@
+package com.frostmaster.leetcode.ReverseInteger;
+
+public class Solution {
+
+  public int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+      int tail = x % 10;
+      if(x > 0 && (Integer.MAX_VALUE - tail) / 10 < result)
+        return 0;
+      if(x < 0 && (Integer.MIN_VALUE - tail) / 10 > result)
+        return 0;
+      x /= 10;
+      result = result * 10 + tail;
+    }
+
+    return result;
+  }
+
+  public int isBiggerTHanInt(int x, int tail, int result) {
+    if(x > 0 && (Integer.MAX_VALUE - tail) / 10 < result)
+      return 0;
+    if(x < 0 && (Integer.MIN_VALUE - tail) / 10 > result)
+      return 0;
+    return x;
+  }
+
+
+
+}
