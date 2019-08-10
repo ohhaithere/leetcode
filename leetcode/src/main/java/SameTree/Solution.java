605e5103cee90f998c6c743e12320461fa3a0b97
ohhaithere <frostmasterwork@gmail.com>
2019-08-10 21:17:20 +0300

    Same tree solution

---
+++ leetcode/src/main/java/SameTree/Solution.java
@@ -0,0 +1,25 @@
+package SameTree;
+
+class Solution {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+      // one of p and q is null
+    if (q == null || p == null) {
+      return false;
+    }
+    if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.right, q.right) &&
+          isSameTree(p.left, q.left);
+    }
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}
