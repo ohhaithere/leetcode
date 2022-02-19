67717fbc710e1d2cf25e068d7909339b46bf8e0d
ohhaithere <frostmasterwork@gmail.com>
2022-02-19 15:09:21 +0300

    3sum

---
+++ leetcode/src/main/java/ThreeSum/Solution.java
@@ -0,0 +1,38 @@
+package ThreeSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum==0){
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }
+
+        return new ArrayList<>(set);
+    }
+
+}
