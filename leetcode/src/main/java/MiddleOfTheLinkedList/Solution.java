6b1b0979a559b90eff06db30f7cde0f7ba3c43f5
ohhaithere <frostmasterwork@gmail.com>
2022-03-22 13:29:24 +0300

    MiddleOfTheLinkedList

---
+++ leetcode/src/main/java/MiddleOfTheLinkedList/Solution.java
@@ -0,0 +1,23 @@
+package MiddleOfTheLinkedList;
+
+public class Solution {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while (head != null) {
+            A[t++] = head;
+            head = head.next;
+        }
+        return A[t / 2];
+    }
+
+}
