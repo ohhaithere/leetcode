6944aba49fd84d34b34f685265eaa1bc6a338998
ohhaithere <frostmasterwork@gmail.com>
2022-03-31 17:09:37 +0300

    Invert binary tree

---
+++ leetcode/src/main/java/InvertBinaryTree/Solution.java
@@ -0,0 +1,34 @@
+package InvertBinaryTree;
+
+public class Solution {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public TreeNode invertTree(TreeNode root) {
+        return recur(root);
+    }
+
+    public TreeNode recur(TreeNode root){
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        root.left = recur(root.left);
+        root.right = recur(root.right);
+        return root;
+    }
+
+}
